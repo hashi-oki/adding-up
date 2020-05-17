929699589da1294160024eef908fc01c8fe48667
hashi-oki <sushikuite77@gmail.com>
2020-05-17 12:08:29 +0900

    ランキングの実装完了

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 'use strict';
+
+//ファイルを一行ずつ読み込む
+//モジュール読み込み
+const fs = require('fs');
+const readline = require('readline')
+//csvからStreamを生成、rlオブジェクト作成
+const rs = fs.createReadStream('./popu-pref.csv');
+const rl = readline.createInterface({ input: rs, output: {} });
+
+rl.on('line', linerString => {
+    const columns = linerString.split(',');
+    const year = parseInt(columns[0]);
+    const prefecture = columns[1];
+    const popu = parseInt(columns[3]);
+    if (year === 2010 || year === 2015){
+        console.log(year);
+        console.log(prefecture);
+        console.log(popu);
+    }
+})
+
